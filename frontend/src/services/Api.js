917ea73730cfbcb445ed95def4bff9b521ff0328
PendicGordan <gordan.pendic5@gmail.com>
2020-01-22 01:02:18 +0100

    Converting queries finished

--- frontend/src/services/Api.js
+++ frontend/src/services/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default () => {
     return axios.create({
-        baseURL: 'http://localhost:3000/v1/',
+        baseURL: 'http://localhost:8080/api',
         headers: {
             'Access-Control-Allow-Origin': '*',
             'content-type': 'application/json'
